071a82b57fbd3906ee95d68ff8db5b9d59a52584
Nikša Fantela <niksa.simple.stack@gmail.com>
2019-11-19 13:05:34 +0100

    started data source

--- lib/table/containers/table/table.component.ts
+++ lib/table/containers/table/table.component.ts
@@ -1,7 +1,9 @@
-import { Component, ChangeDetectionStrategy, AfterContentChecked, OnInit, OnDestroy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, AfterContentChecked, OnInit, OnDestroy, IterableDiffers, ChangeDetectorRef, ElementRef, Attribute, Optional, Inject, ViewChild, Input } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { DataRowOutlet, HeaderRowOutlet, FooterRowOutlet } from 'lib/table/directives/row.directives';
 
 @Component({
-    selector: 'table',
+    selector: 'ang-table table[ang-table]',
     styleUrls: ['table.component.scss'],
     template: `
         <ng-content select="caption"></ng-content><!-- caption has to always be first -->
@@ -9,12 +11,42 @@ import { Component, ChangeDetectionStrategy, AfterContentChecked, OnInit, OnDest
         <ng-container rowOutlet></ng-container>
         <ng-container footerRowOutlet></ng-container>
     `,
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.Default
 })
 export class TableComponent<T> implements AfterContentChecked, OnInit, OnDestroy{
 
-    // type T will be used for data tdype later
-    constructor() {}
+    constructor(
+        private readonly _differs: IterableDiffers,
+        private readonly _changeDetectorRef: ChangeDetectorRef,
+        private readonly _elementRef: ElementRef, 
+        @Attribute('role') role: string, // collect attribubte role if any
+        @Inject(DOCUMENT) _document: any) {
+            if (!role) {
+                this._elementRef.nativeElement.setAttribute('role', 'grid');
+            }
+        this._document = _document;
+        this._isNativeHtmlTable = this._elementRef.nativeElement.nodeName === 'TABLE';
+    }
+
+    private _document: Document;
+    private _isNativeHtmlTable: boolean;
+
+    // Outlets in the table's template where the header, data rows, and footer will be inserted.
+    @ViewChild(DataRowOutlet, {static: true}) rowOutlet: DataRowOutlet;
+    @ViewChild(HeaderRowOutlet, {static: true}) headerRowOutlet: HeaderRowOutlet;
+    @ViewChild(FooterRowOutlet, {static: true}) footerRowOutlet: FooterRowOutlet;
+
+    /*========================= INPUTS ==============================*/
+    @Input()
+    get dataSource(): CdkTableDataSourceInput<T> {
+      return this._dataSource;
+    }
+    set dataSource(dataSource: CdkTableDataSourceInput<T>) {
+      if (this._dataSource !== dataSource) {
+        this._switchDataSource(dataSource);
+      }
+    }
+    private _dataSource: CdkTableDataSourceInput<T>;
     
     ngAfterContentChecked(): void {
         // TODO
--- lib/table/directives/row.directives.ts
+++ lib/table/directives/row.directives.ts
@@ -3,25 +3,17 @@ import { Directive, ViewContainerRef, ElementRef } from '@angular/core';
 interface RowOutlet {
     viewContainer: ViewContainerRef;
 }
-/**
- * Provides a handle for the table to grab the view container's ng-container to insert data rows.
- */
+
 @Directive({selector: '[rowOutlet]'})
 export class DataRowOutlet implements RowOutlet {
   constructor(public viewContainer: ViewContainerRef, public elementRef: ElementRef) {}
 }
 
-/**
- * Provides a handle for the table to grab the view container's ng-container to insert the header.
- */
 @Directive({selector: '[headerRowOutlet]'})
 export class HeaderRowOutlet implements RowOutlet {
   constructor(public viewContainer: ViewContainerRef, public elementRef: ElementRef) {}
 }
 
-/**
- * Provides a handle for the table to grab the view container's ng-container to insert the footer.
- */
 @Directive({selector: '[footerRowOutlet]'})
 export class FooterRowOutlet implements RowOutlet {
   constructor(public viewContainer: ViewContainerRef, public elementRef: ElementRef) {}
