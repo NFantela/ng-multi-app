26a1a6ade6e14771fedbd0fc3343bc852d2fa0c5
Nikša Fantela <niksa@robotiq.ai>
2020-04-28 08:24:48 +0200

    Updated to Angular v9

--- lib/angular-calendar/containers/angular-calendar/angular-calendar.component.ts
+++ lib/angular-calendar/containers/angular-calendar/angular-calendar.component.ts
@@ -125,7 +125,7 @@ export class AngularCalendarComponent implements OnDestroy, OnInit{
     this.dateData.complete();
   }
 
-  @ContentChild(TemplateRef, {static:false}) eventTemplate: TemplateRef<any>;
+  @ContentChild(TemplateRef) eventTemplate: TemplateRef<any>;
 
   /* depending on current selectedView calculate start and end dates for that time period */
   private _createStartEndDates(date:Date){
